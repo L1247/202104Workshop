66d1167c50bc000f0f549d672f3e122d676f8a67
rStar <starlove124760@gmail.com>
2021-04-24 18:07:55 +0800

    feat: add Test - Should_Throw_Exception by wrong number input

--- Assets/Workshop/Scripts/Main/StarMath.cs
+++ Assets/Workshop/Scripts/Main/StarMath.cs
@@ -1,9 +1,14 @@
+using System;
+
 namespace Main
 {
     public class StarMath
     {
         public int Add(int number1 , int number2)
         {
+            if (number1 < 0 & number2 < 0) throw new Exception("number1 and number2 are negative number");
+            if (number1 < 0) throw new Exception("number1 is negative number");
+            if (number2 < 0) throw new Exception("number2 is negative number");
             var result = number1 + number2;
             return result;
         }
--- Assets/Workshop/Scripts/Tests/StarMathTests.cs
+++ Assets/Workshop/Scripts/Tests/StarMathTests.cs
@@ -1,22 +1,41 @@
+using System;
 using Main;
 using NUnit.Framework;
+using UnityEngine;
 
 
 namespace MainTest
 {
     public class StarMathTests
     {
+        private StarMath _starMath;
+
+        [SetUp]
+        public void Setup()
+        {
+            _starMath = new StarMath();
+        }
+
         [Test]
         [TestCase(1 , 2 , 3)]
         [TestCase(789 , 123 , 912)]
+        [TestCase(0 , 0 , 0)]
         public void Should_Return_Positive_Number_Of_Add_Result(int number1 , int number2 , int expected)
         {
-            // Arrange
-            var starMath = new StarMath();
             // Act
-            var addResult = starMath.Add(number1 , number2);
+            var addResult = _starMath.Add(number1 , number2);
             // Assert
             Assert.AreEqual(expected , addResult);
+        }
+
+        [Test]
+        [TestCase(-1 , 0 , "number1 is negative number")]
+        [TestCase(0 , -1 , "number2 is negative number")]
+        [TestCase(-1 , -1 , "number1 and number2 are negative number")]
+        public void Should_Throw_Exception(int number1 , int number2 , string errorMessage)
+        {
+            var exception = Assert.Throws<Exception>(() => { _starMath.Add(number1 , number2); });
+            Assert.AreEqual(errorMessage , exception.Message);
         }
     }
 }
