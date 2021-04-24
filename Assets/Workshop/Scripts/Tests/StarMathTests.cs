50cdbd7338aa33f3fab70d6d64dde07c030f550d
rStar <starlove124760@gmail.com>
2021-04-24 17:29:34 +0800

    refactor: introduce variable

--- Assets/Workshop/Scripts/Tests/StarMathTests.cs
+++ Assets/Workshop/Scripts/Tests/StarMathTests.cs
@@ -1,10 +1,11 @@
 using NUnit.Framework;
+using UnityEngine;
 
 public class StarMathTests
 {
     [Test]
-    [TestCase(1,2,3)]
-    [TestCase(789,123,912)]
+    [TestCase(1 , 2 , 3)]
+    [TestCase(789 , 123 , 912)]
     public void Should_Return_Positive_Number_Of_Add_Result(int number1 , int number2 , int expected)
     {
         // Arrange
@@ -21,6 +22,7 @@ public class StarMath
 {
     public int Add(int number1 , int number2)
     {
-        return number1 + number2;
+        var result = number1 + number2;
+        return result;
     }
 }
